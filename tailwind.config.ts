cf93fd549800ae1470f150088f1f2277eec8ea5c
Sebastian Guerrero <wesdell17@gmail.com>
2026-04-06 10:00:05 -0500

    Remove unnecessary tailwind plugin and upload static media

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -145,7 +145,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), "prettier-plugin-tailwindcss"],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
