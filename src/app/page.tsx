b1f2094016f3a03265f46402581a8faa0979de3c
Sebastian Guerrero <wesdell17@gmail.com>
2026-04-07 09:38:11 -0500

    Add Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Landing from "@/app/(nondashboard)/landing/page";
+
+
 export default function Home() {
   return (
-    <div>
-      Hello, World!
+    <div className="nondashboard-layout">
+      <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
     </div>
   );
 }
